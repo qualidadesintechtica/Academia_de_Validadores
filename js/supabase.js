7a027d47d11fd585dde8a94166136997f3e2cc40
animaligia <qualidadesintechtica@animaeducacao.com.br>
2026-08-06 13:35:22 -0300

    060820263

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,12 @@
-const SUPABASE_URL = "https://xgtzyinfinjehxzojafn.supabase.co";
-const SUPABASE_PUBLIC_KEY = "sb_publishable_4iYSQaPGuSxaaZUkwdF_lw_IueIdhRw";
+const SUPABASE_URL =
+  "https://xgtzyinfinjehxzojafn.supabase.co";
 
-const supabaseClient = window.supabase.createClient(
+const SUPABASE_PUBLIC_KEY =
+  "sb_publishable_4iYSQaPGuSxaaZUkwdF_lw_IueIdhRw";
+
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_PUBLIC_KEY
+  SUPABASE_PUBLIC_KEY,
 );
+
+console.log("Supabase conectado com sucesso:", window.supabaseClient);
