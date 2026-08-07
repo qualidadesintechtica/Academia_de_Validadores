708dfb36fc79de3edb8ec04cc636c30cbd50522c
animaligia <qualidadesintechtica@animaeducacao.com.br>
2026-08-07 14:53:58 -0300

    academia V4$final

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.ACADEMIA_CONFIG={
-SUPABASE_URL:"https://xgtzyinfinjehxzojafn.supabase.co/rest/v1/",
+SUPABASE_URL:"https://xgtzyinfinjehxzojafn.supabase.co",
 SUPABASE_ANON_KEY:"sb_publishable_4iYSQaPGuSxaaZUkwdF_lw_IueIdhRw",
 SITE_URL:"https://qualidadesintechtica.github.io/Academia_de_Validadores/",
 TOTAL_MODULOS:7,
